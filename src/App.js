6fbe09e495c108d6d357460fc7cd7e034537201c
AjinkyaPradhan1 <virat@gmail.com>
2023-03-16 16:31:54 +0530

    Counter App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
 
 function App() {
+  const [count,setCount] = useState(0);
+
+  const increment = ()=>{
+    setCount(count + 1);
+    console.log("Button clcked");
+  }
+
+  const decrement = ()=>{
+    setCount(count - 1);
+
+    if(setCount<0){
+      return;
+    }
+  }
+
   return (
     <div className="App">
-      
+      <div className="main">
+        <div className="content">
+          <div className="top">
+            <div className='top_content'>{count}</div>
+          </div>
+          <div className="bottom">
+            <div className='bottom_content'>
+              <button className='bt1' onClick={increment}>+</button>
+              <button className='bt1'  onClick={decrement}>-</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
